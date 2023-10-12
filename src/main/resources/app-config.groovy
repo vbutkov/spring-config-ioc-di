14e67ca1fa59955db3d688ed12a658a32826edcb
vbutkov <bytkov@mail.ru>
2023-10-12 14:29:38 +0300

    Changed the SecondBean scope to prototype

--- src/main/resources/app-config.groovy
+++ src/main/resources/app-config.groovy
@@ -2,8 +2,10 @@ import ru.vbutkov.spring_ioc_di.bean.FirstBean
 import ru.vbutkov.spring_ioc_di.bean.SecondBean
 import ru.vbutkov.spring_ioc_di.bean.ThirdBean
 
-beans{
-    secondBean(SecondBean)
+beans {
+    secondBean(SecondBean) { bean ->
+        bean.scope = 'prototype'
+    }
 
     firstBean(FirstBean, secondBean)
 
